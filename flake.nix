2b80c2eebfc095fd47fd09c89b63071213b22c19
2814109 <2814109k@gmail.com>
2023-11-26 01:51:31 +0900

    update

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    inputs = {
+        ihp.url = "github:digitallyinduced/ihp/v1.2";
+        nixpkgs.follows = "ihp/nixpkgs";
+        flake-parts.follows = "ihp/flake-parts";
+        devenv.follows = "ihp/devenv";
+        systems.follows = "ihp/systems";
+    };
+
+    outputs = inputs@{ ihp, flake-parts, systems, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+
+            systems = import systems;
+            imports = [ ihp.flakeModules.default ];
+
+            perSystem = { pkgs, ... }: {
+                ihp = {
+                    enable = true;
+                    projectPath = ./.;
+                    packages = with pkgs; [
+                        # Native dependencies, e.g. imagemagick
+                    ];
+                    haskellPackages = p: with p; [
+                        # Haskell dependencies go here
+                        p.ihp
+                        cabal-install
+                        base
+                        wai
+                        text
+                    ];
+                };
+            };
+
+        };
+}
